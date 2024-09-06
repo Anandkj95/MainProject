c66a06834b0a99bf1f038522246ade0ee43639d7
Anand <jaysonanandk@gmail.com>
2024-09-07 01:00:32 +0530

    file6 first commit

---
+++ file6.cs
@@ -0,0 +1,10 @@
+class File6{
+    static int a=10;
+    static int b=29;
+    static int c;
+    public static void Main(string[] args)
+    {
+        c=a+b;
+        Console.WriteLine("value c " + c);
+    }
+}
